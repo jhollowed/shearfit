3e8107a8b1c75571dc6e5967d65085eba9f92464
Joe Hollowed <jphollowed@gmail.com>
2019-03-13 14:05:32 -0500

    renaming src dir

--- gammaProf/__init__.py
+++
@@ -1,8 +0,0 @@
-# import package classes and utility functions
-from gammaProf import cluster
-from gammaProf.analytic_profiles import NFW_shear_profile
-from gammaProf.mass_concentration import child18
-
-# define attributes
-__version__ = '0.1'
-__author__ = 'Joe Hollowed <jphollowed@anl.gov>'
