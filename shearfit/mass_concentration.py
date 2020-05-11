d09333eccde26b7ede9587566cf7564c65dd04ce
Joe Hollowed <jphollowed@gmail.com>
2020-05-11 08:37:37 -0500

    missing factor of h in child cM function

--- shearfit/mass_concentration.py
+++ shearfit/mass_concentration.py
@@ -7,7 +7,7 @@ This module contains a collection of concentration-mass relations as given in th
 the functions below take a halo mass as an argument, and will return a predicted concentration.
 '''
 
-def child2018(m200c_h, z, cosmo, fit='nfw_stack'):
+def child2018(m200c, z, cosmo, fit='nfw_stack'):
     """
     Computes the predicted halo concentration, given a M_200c halo mass, using the 
     c-M relation of Child et.al. 2018, as implemented in COLOSSUS by Diemer.
@@ -15,7 +15,7 @@ def child2018(m200c_h, z, cosmo, fit='nfw_stack'):
     Parameters
     ----------
     m200c : float 
-        The halo's mass :math:`M_{200c}` in units of :math:`M_{\\odot} h^{-1}`.
+        The halo's mass :math:`M_{200c}` in units of :math:`M_{\\odot}` (no h dependence).
     z : float
         The halo redshift
     cosmo : `astropy` `cosmology` object instance
@@ -32,9 +32,11 @@ def child2018(m200c_h, z, cosmo, fit='nfw_stack'):
     """
     
     # draw a concentration from gaussian with scale and location defined by Child+2018
+    # colossus expects input mass with h^(-1) dependence, so multiply through before passing
     cosmo_colossus = colcos.setCosmology('OuterRim',
                      {'Om0':cosmo.Om0, 'Ob0':cosmo.Ob0, 'H0':cosmo.H0.value, 'sigma8':0.8, 
                       'ns':0.963, 'relspecies':False})
+    m200c_h = m200c * cosmo.h
     c200c = mass_conc(m200c_h, '200c', z, model='child18')
     c_err = c200c/3
 
